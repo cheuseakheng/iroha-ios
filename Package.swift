3775da575b8972b5ba389df002ac9b08cf3fd11f
Seakheng Cheu <81610713+cheuseakheng@users.noreply.github.com>
2025-07-09 11:33:58 +0700

    Update Package.swift
    Signed-off-by: Seakheng Cheu <81610713+cheuseakheng@users.noreply.github.com>

--- Package.swift
+++ Package.swift
@@ -2,21 +2,39 @@
 import PackageDescription
 
 let package = Package(
-    name: "IrohaCrypto",
+    name: "IrohaCommunication",
     platforms: [
         .iOS(.v12)
     ],
     products: [
-        .library(name: "IrohaCrypto", targets: ["IrohaCrypto"])
+        .library(
+            name: "IrohaCommunication",
+            targets: ["IrohaCommunication"]
+        )
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.21.0"),
+        .package(url: "https://github.com/grpc/grpc-swift.git", from: "1.10.0")
     ],
     targets: [
         .target(
-            name: "IrohaCrypto",
-            path: "IrohaCrypto",
-            publicHeadersPath: ".", // <- header is inside IrohaCrypto/
+            name: "IrohaCommunication",
+            dependencies: [
+                .product(name: "SwiftProtobuf", package: "swift-protobuf"),
+                .product(name: "GRPC", package: "grpc-swift")
+            ],
+            path: "IrohaCommunication/Classes",
+            exclude: ["main.m", "Info.plist"], // optional
+            publicHeadersPath: ".",
             cSettings: [
-                .headerSearchPath(".")
+                .headerSearchPath("../../ProtoGen")
             ]
+        ),
+        .target(
+            name: "ProtoGen",
+            dependencies: [],
+            path: "ProtoGen",
+            publicHeadersPath: "."
         )
     ]
 )
